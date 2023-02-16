270326f64b0ed20ac12f446a2807a4239737d00a
AnnaRoz <anna.rozumova108@gmail.com>
2023-02-16 22:18:39 +0100

    feat: updates from play_with_people repository and linting

--- human.py
+++ human.py
@@ -1,5 +1,96 @@
-# new file with nothing
+'''
+Task:
+- this module play_with_people we will need later, so please keep it, just rename it to human.py
+- create another module, which will contain class Shop(),
+ which will have variables:
+ => 1.string name of the shop
+ => 2.dictionary will contain 3 items and their price and number of the pieces in the shop
+ - the name of the shop and the dictionary will come as input when creating instance of the class Shop()
+ dictionaries example:
+ products = {rohlik: {cena: 3, kusu: 100}, chleba:  {cena: 25, kusu: 100}, ryze:  {cena: 30, kusu: 100}}
+ - there will be method where you can buy something from the shop (so input will be name of product and number of wanted pieces)
+ => and in that case it will reduce the products number available in the shop and also return the price for that specific buying
+ (and later for next task => when there will not be enough products, it will return "Sorry, we don't have enough")
+'''
 
 class Human():
-    __init__(self):   
-        pass
+    type_of_animal = 'original'  # this is class variable
+    def __init__(self, age, sex, first_name, last_name, lenght_in_cm, hair_colour):
+        self.age = age
+        self.sex = sex
+        self.first_name = first_name
+        self.last_name = last_name
+        self.length = lenght_in_cm
+        self.hair_colour = hair_colour
+        self.phone_number = None
+        self.address = None
+
+    def get_hair_colour(self):
+        hair_colour_to_be_returned = self.hair_colour
+        return hair_colour_to_be_returned
+
+    def get_full_name(self):
+        full_name = self.first_name + ' ' + self.last_name
+        return full_name
+
+    def _age_in_days(self): # when method is used only in class and it is not used publicly (outside of the class), it starts with _
+        return 365 * self.age
+
+    def get_age_in_days(self):
+        return self._age_in_days()
+
+    def get_details(self):
+        return f'{self.type_of_animal} {self.get_full_name()}'
+
+    def phone_adress(self, human_number, human_adress):
+        self.phone_number = human_number
+        self.address = human_adress
+
+    def get_phone_adress(self):
+        return f'The adress of {self.get_full_name()} is {self.address} and phone number is {self.phone_number}'
+
+if __name__ == "__main__":
+    human_1 = Human(6, 'man', 'Stepan', 'Rozum', 120, 'brown')
+    human_2 = Human(4, 'woman', 'Naomi', 'Rozumova', 100, 'blond')
+'''
+This is test for commit change in comment
+hair_colour_of_human_1 = human_1.get_hair_colour()
+hair_colour_of_human_2 = human_2.get_hair_colour()
+hair_colour_of_human_1_another_way = human_1.hair_colour
+print(hair_colour_of_human_1)
+print(hair_colour_of_human_1_another_way)
+print(hair_colour_of_human_2)
+print('Lenght of human_1:', human_1.length, ' centimeters')
+print('Lenght of human_2:', human_2.length, ' centimeters')
+length_of_stopik_plus_naomi = human_1.length + human_2.length
+print('Stopik + Naomi length is:', length_of_stopik_plus_naomi, ' centimeters')
+print('Specific human first name:', human_1.first_name)
+print('Specific human last name:', human_1.last_name)
+print('Specific human full name:', human_1.get_full_name())
+human_1.first_name = 'Stopinek'
+print('Changed first name of human_1:', human_1.first_name)
+print('Specific human full name after change:', human_1.get_full_name())
+print(f'Print age of {human_1.get_full_name()} is {human_1.get_age_in_days()}')
+print(f'Print age of {human_2.get_full_name()} is {human_2.get_age_in_days()}')
+print(human_1.type_of_animal)
+print(human_2.type_of_animal)
+Human.type_of_animal = 'tiger'
+print(human_1.type_of_animal)
+print(human_2.type_of_animal)
+print(human_1.get_details())
+print(human_2.get_details())
+print(human_1.type_of_animal)
+human_1.type_of_animal = 'human'
+print(human_1.type_of_animal)
+print(human_2.type_of_animal)
+print(Human.type_of_animal)
+Human.type_of_animal = 'original'
+print(human_1.type_of_animal)
+print(human_2.type_of_animal)
+print(Human.type_of_animal)
+
+
+human_1.phone_adress('7774568', 'Manetinska 43')
+print(human_1.get_phone_adress())
+
+'''
